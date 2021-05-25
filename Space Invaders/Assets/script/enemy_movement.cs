d4eaa152995abcfc29145d9104dd901e1bf0efd0
rickdohmen <70955143+rickdohmen@users.noreply.github.com>
2021-05-25 12:11:58 +0100

    edit ignore box collider and enemy movement

---
+++ Space Invaders/Assets/script/enemy_movement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_movement : MonoBehaviour
+{
+
+    public float speed;
+    private float distance; //set the distance of the enemy seeing the player
+    public float agroRange;
+    public Transform target;//set target from inspector instead of looking in Update
+
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+        if (Vector2.Distance(transform.position, target.position) < agroRange) //Agro range
+        {  //rotate to look at the player
+            transform.LookAt(target.position);
+            transform.Rotate(new Vector2(0, -90), Space.Self);//correcting the original rotation
+        }
+
+        if (Vector2.Distance(transform.position, target.position) < agroRange) //Agro range
+        {   //move towards the player
+            if (Vector2.Distance(transform.position, target.position) > distance)
+            {//move if distance from target is greater than distance
+                transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+
+            }
+        }
+
+    }
+}
+
