2b94d2b741ef0ddb038f569699249781dce7d4e8
KevinAssink <31471104+KevinAssink@users.noreply.github.com>
2021-06-02 12:57:25 +0200

    made the menu's work,
    still some minor errors/warnings but working on a fix

---
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    public float speed;
+    private float distance; //set the distance of the enemy seeing the player
+    public float agroRange;
+    public Transform target;//set target from inspector instead of looking in Update
+    public int health = 100; // amount of base health
+    public GameObject deathEffect; // plays death effect 
+    public Transform enemyGFX;
+    Rigidbody2D rb;
+
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+        if (Vector2.Distance(transform.position, target.position) < agroRange) //Agro range
+        {  //rotate to look at the player
+            //transform.LookAt(target.position);
+            //transform.Rotate(new Vector2(0, -90), Space.Self);//correcting the original rotation
+        }
+
+        if (Vector2.Distance(transform.position, target.position) < agroRange) //Agro range
+        {   //move towards the player
+            if (Vector2.Distance(transform.position, target.position) > distance)
+            {//move if distance from target is greater than distance
+                if (transform.position.x < target.position.x)
+                transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+                else
+                    transform.Translate(new Vector2(-speed * Time.deltaTime, 0));
+
+            }
+        }
+
+        if (rb.velocity.x >= 0.01f)
+        {
+            enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
+
+        }
+            else if (rb.velocity.x <= -0.01f)
+        {
+            enemyGFX.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/controlplayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlplayer : MonoBehaviour
+{
+
+    float speed = 4f;
+    public Animator Animator;
+    private Rigidbody2D rb;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // What is the player doing with the controls?
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"), 0);
+        //rb.velocity = new Vector2(move, 0);
+        rb.AddForce(move);
+
+        // Update the ships position each frame
+        transform.position += move
+            * speed * Time.deltaTime;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        Animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (horizontalMove > 0f)
+        {
+            rb.velocity = new Vector2 (horizontalMove * speed, rb.velocity.y);
+            transform.localScale = new Vector2(1f, 1f);
+        }
+        else if (horizontalMove < 0f)
+        {
+            rb.velocity = new Vector2 (horizontalMove * speed, rb.velocity.y);
+            transform.localScale = new Vector2(-1f, 1f);
+        }
+    }
+}
