5a0d34186209671f19c560d50a5532716c2aa7bf
mennoweerman <70953228+mennoweerman@users.noreply.github.com>
2021-06-22 13:17:50 +0200

    Merged conflicten

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class bullet : MonoBehaviour
 {
+
+    public float velX = 5f;
+    float velY = 0f;
+    Rigidbody2D rb;
+    public int destroyTime = 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(velX, velY);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D hitinfo)
     {
         
+        Destroy(gameObject, 3f);
+    }
+
+
+    void Update()
+    {
+        Destroy(gameObject, destroyTime);
     }
 }
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
-{	// Use this for initialization
-	public Transform firePoint;
-	public GameObject bulletPrefab;
+public class weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (Input.GetButtonDown("Fire1"))
-		{
-			Shoot();
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
 
-	void Shoot()
-	{
-		Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-	}
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
 }
